3955601dc21a2b9be13128652e256bcdbd5574ff
Chelsi Jones <cajones.bz@gmail.com>
2017-06-08 09:47:42 -0500

    *Now* the finished product.

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,25 +1,43 @@
-
-def merge_sort(arr, b=[])
-	if arr.length == 1 && arr.nil? == false
-		b << arr[0] if b.empty? or arr[0] >= b[-1]
-		return b.insert(0, arr[0]) if arr[0] < b[0]
-		b.each_with_index do |n, index|
-			return b.insert(index, arr[0]) if arr[0] < n
-		end
+def merge(arr1, arr2, sorted=[])
+	if arr1.empty? and arr2.empty?
+		sorted
 	else
+		if arr2.empty? or (arr1[0] < arr2[0] if arr1[0].is_a? Integer)
+			sorted << arr1[0]
+			arr1.shift
+			merge(arr1, arr2, sorted)
+		else
+			sorted << arr2[0]
+			arr2.shift
+			merge(arr1, arr2, sorted)
+		end
+	end
+end
+
+def merge_sort(arr, x=nil, b=[])
+	x ||= arr.length
+	if arr.length == 1
+		b << arr
+	else arr.length > 1
 		left = arr.slice(0..(arr.length/2 - 1))
 		right = arr.slice(arr.length/2..-1)
-		merge_sort(left, b)
-		merge_sort(right, b)
+		merge_sort(left, x, b)
+		merge_sort(right, x, b)
 	end
-	b.inspect
+
+	if b.length == x && arr.length == x
+		while b.length > 1
+			i = 0
+			while i < b.length
+				b << merge(b[0], b[1])
+				2.times { b.shift }
+				i += 2
+			end
+		end
+		b[0].inspect
+	end
 end
 
 
-a = [8, 4, 1, 4, 6, 3, 9, 2]
-puts a.inspect
+a = [8, 4, 1, 4, 6, 3, 9, 2, 7]
 puts merge_sort(a)
-puts
-c = [rand(1..10), rand(1..10), rand(1..10), rand(1..10), rand(1..10), rand(1..10), rand(1..10), rand(1..10)]
-puts c.inspect
-puts merge_sort(c)
